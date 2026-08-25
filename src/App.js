a4a23abd96ef1897e631453a9175e56277b5f4f4
Fiddel Omondi <fiddel-omondi@Linux-dev.strathmore.local>
2026-08-25 09:41:54 +0300

    Lifting state up

--- src/App.js
+++ src/App.js
@@ -1,54 +1,28 @@
-import { useState } from "react";
-function Square() {
-  const [valeu, setValue] = useState(null);
-  function handleClick() {
-    setValue("x");
-  }
-  return (
-    <button className="square" onClick={handleClick}>
-      {valeu}
-    </button>
-  );
+import { useState } from 'react';
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
-    <Greet/>
-   
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
-
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
-
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />  
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
-
-     
-    
-    
     </>
   );
-}
-
-function Greet(){
-  return (
-    <>
-    <h1>Hello User welcome to tictactoe game</h1>
-
-    </>
-  );
-
-  
-
 }
