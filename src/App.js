66c6baa33062701006158a916dbe6c5ec391c71a
fiddel-cmd <Fiddel.owuor@starthmore.edu>
2026-08-28 22:15:11 +0300

    complete function to find a winner

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { useState } from 'react';
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
-
+// Board function
 export default function Board() {
   const [xIsnext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
-    if(squares[i]){
+    if (squares[i]) {
+      return;
+    }
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice();
@@ -18,13 +21,24 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    
+
 
     setSquares(nextSquares);
     setXIsNext(!xIsnext);
   }
+  // get the winner and store the result to the variable winner
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status ="Winner " + winner;
+  }
+  else{
+    "Next winner" +(xIsnext ? "X" : "O");
+  }
+  
   return (
     <>
+    <div className = "status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -43,6 +57,25 @@ export default function Board() {
     </>
   );
 }
-function calculateWinner(){
-  
+
+// Function to calculate the winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+
 }
